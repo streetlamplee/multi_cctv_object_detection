42fa7fd12b0c28809d10d5c6d50eabba5b8ee6f1
streetlamplee <jinintg@gmail.com>
2025-09-08 14:04:24 +0900

    feat: 채널/기능 당 thread 처리 >> 채널 당 thread 처리로 변경

--- include/configHandler.h
+++ include/configHandler.h
@@ -1,13 +1,11 @@
 #pragma once
 
 #include "Handler.h"
-#include "json.hpp"
-using json = nlohmann::json;
 #include "alarm.h"
 #include <stack>
 
 
-int read_config_json(std::string json_path, json& config);
+// int read_config_json(std::string json_path, json& config);
 
 
 
