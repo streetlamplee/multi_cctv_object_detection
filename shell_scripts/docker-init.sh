cb56ef29b9c979be1fc3ae7de9f42ad60894b382
streetlamplee <jinintg@gmail.com>
2025-12-11 09:03:12 +0900

    feat: 모델 최신화

--- shell_scripts/docker-init.sh
+++ shell_scripts/docker-init.sh
@@ -1,5 +1,6 @@
 docker run --privileged --rm tonistiigi/binfmt --install arm64
 docker run --rm -v "$(pwd)":/app j4125_cctv /bin/bash -c "\
+    export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH; \
     rm -rf build; \
     mkdir build; \
     cd build; \
